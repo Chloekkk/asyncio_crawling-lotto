6d4680b809fddf41f9725c0cd68a2d96199fe5bf
Chloekkk <skdusdl8804@gmail.com>
2020-02-05 01:55:03 +0900

    asyncio final results

--- lotto/asynio.py
+++ lotto/asynio.py
@@ -2,29 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 import asyncio
 import time
+from collections import ChainMap
+
+urls = ["https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo="+i
+            for i in list(list(map(str, range(1, 5))))] # 5 -> 811 로 변경
 
 s = time.time()
 results = []
-# async def get_request(url):
-#     try:
-#         return requests.get(url, timeout=10, headers={"User-Agent": "Mozilla/5.0"})
-#     except:
-#         get_request(url)
+def get_request(url):
+    try:
+        return requests.get(url, timeout=10, headers={"User-Agent": "Mozilla/5.0"})
+    except:
+        get_request(url)
 
-async def get_page(url):
-    req = await loop.run_in_executor(None, requests.get, url)
+async def get_page(url, index):
+    season = index + 1
+    req = await loop.run_in_executor(None, get_request, url)
     html = req.text
     soup = await loop.run_in_executor(None, BeautifulSoup, html, 'html.parser')
-    return soup
+
+    winning_numbers = []
+    numbers = soup.select("span.ball_645.lrg")
+    for num in numbers:
+        winning_numbers.append(num.text)
+    return {season:winning_numbers}
 
 async def main():
-    urls = ["https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo="+i
-            for i in list(list(map(str, range(1, 5))))]
-    # 5 -> 811 로 변경
-    fts = [asyncio.ensure_future(get_page(u)) for u in urls]
+    fts = [asyncio.ensure_future(get_page(u, urls.index(u))) for u in urls]
     result = await asyncio.gather(*fts)
     global results
-    results = result
+    # convert list of dict to one dict
+    results = dict(ChainMap(*result))
+    print(results)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
