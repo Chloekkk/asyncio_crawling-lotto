eb8fd3f411aaa84ab41ae7f8fa550d69920d8cbb
Chloekkk <skdusdl8804@gmail.com>
2020-02-07 00:19:11 +0900

    check crawling data from views.py

--- lotto/views.py
+++ lotto/views.py
@@ -12,7 +12,7 @@ def result(request):
     
     numberlist = []
     # lottolist = list(range(1,46))
-    lottolist = random.sample(list(range(1, 46)), 6)
+    lottolist = random.sample(list(range(1, 46)), 7)
     # 왜 sort 안됨?
 
     # get input data
@@ -39,9 +39,24 @@ def result(request):
         if num in lottolist:
             sum += 1
     
-    print("hello")
-    print(asyncio_crawling.results)
-    results = asyncio_crawling.results
+    # print("hello")
+    # print(asyncio_crawling.results)
+    results = lotto.asyncio_crawling.results
+    final_last_lotto_num = []
+    final_count = 0
+    for i in range(1, len(results.keys())+1):
+        my_num = set(numberlist)
+        last_lotto_num_count = set(results.get(i)[:-1])
+        final_last_lotto_num_count = set(final_last_lotto_num[:-1])
+        
+        count = len(my_num & last_lotto_num)
+        final_count = len(my_num & final_last_lotto_num_count)
+        if bool(final_last_lotto_num) == False :
+            final_last_lotto_num.append(results.get(i))
+        elif count > final_count:
+            pass # 여기서 부터 머리아파 
+
+
 
     percentage = (sum/6)*100
     return render(request, 'result.html', {'numlist':numberlist, 'lotto':lottolist, 'sum':sum, 'percent':percentage, 'all_winning_numbers': results})
